2b1a70043e19e2547bbcb017289dd966f7f2d10c
shootingcoco <luis.chabot@gmail.com>
2019-01-24 13:14:43 +0100

    '13:14'

--- js/demo.js
+++ js/demo.js
@@ -4,8 +4,25 @@
 
     var init = function(){
         nav_div = document.getElementById('nav');
-        nav_div.parentNode.style.width = (nav_div.offsetWidth+1) + 'px';
+        nav_btn = document.getElementById('nav_btn');
+        nav_c = document.getElementById('nav_c');
+        nav_c2 = document.getElementById('nav_c2');
+        nav_btn.onclick = navBtnClick;
         lc.addEvent(window, 'scroll', onScroll);
+
+        resize(lc._screen.width, lc._screen.height);
+        lc._screen.responsive.push(resize);
+    };
+
+    var navBtnClick = function(){
+        if(!is_min_nav){
+            is_min_nav = true;
+            nav_c2.appendChild(nav_div);
+        }
+        else{
+            is_min_nav = false;
+            nav_c.appendChild(nav_div);
+        }
     };
 
     var onScroll = function(){
@@ -18,10 +35,25 @@
             is_nav_fixed = false;
             document.body.className = document.body.className.replace(' nav_fixed', '');
         }
-    }
+    };
+
+    var resize = function(w, h){
+        if(w > 700){
+            if(is_min_nav) lc.eventFire(nav_btn, 'click');
+            if(isLoad){
+                nav_c.style.width = (nav_div.offsetWidth+1) + 'px';
+                isLoad = false;
+            }
+        }
+    };
 
     var nav_div = null;
+    var nav_btn = null;
+    var nav_c = null;
+    var nav_c2 = null;
+    var is_min_nav = false;
     var is_nav_fixed = false;
+    var isLoad = true;
 
     init();
 
