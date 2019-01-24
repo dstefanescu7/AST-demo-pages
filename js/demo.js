5db6650bc8d7af636f9b4b0dd0535e9d358b8065
shootingcoco <luis.chabot@gmail.com>
2019-01-24 12:28:14 +0100

    'test2'

--- js/demo.js
+++ js/demo.js
@@ -2,4 +2,27 @@
 
 (function(){
 
+    var init = function(){
+        nav_div = document.getElementById('nav');
+        nav_div.parentNode.style.width = (nav_div.offsetWidth+1) + 'px';
+        lc.addEvent(window, 'scroll', onScroll);
+    };
+
+    var onScroll = function(){
+        var st = lc.getScrollTop();
+        if(!is_nav_fixed && st > nav_div.offsetTop){
+            is_nav_fixed = true;
+            document.body.className += ' nav_fixed';
+        }
+        else if(is_nav_fixed && st <= nav_div.parentNode.offsetTop){
+            is_nav_fixed = false;
+            document.body.className = document.body.className.replace(' nav_fixed', '');
+        }
+    }
+
+    var nav_div = null;
+    var is_nav_fixed = false;
+
+    init();
+
 })();
